1fc5e2b02be0451907a44fb2e201bcda64f4c949
Lee Seung Hak <99tmdgkr@gmail.com>
2022-10-11 19:55:34 +0900

    피카츄 좌우 이동

--- Pikachu.py
+++ Pikachu.py
@@ -1,36 +1,63 @@
 from pico2d import *
 
+class Player:
+    def __init__(self):
+        self.pos = [90, 90]   # x, y 위치
+        self.frame = 0
+        self.update_frame = 0   # 입력 딜레이가 0.01이여야 조작감이 좋아서 애니메이션은 딜레이가 0.1이 되도록 하는 변수
+        self.frame_flag = True  # True 면 프레임 +, False 면 프레임 -
+        self.sprite_size = 66
+        self.dir = 0            # +면 우측이동, -면 좌측이동
+    def draw(self):
+        sprite_sheet.clip_draw(self.frame * self.sprite_size,
+                               885 - (266+self.sprite_size),
+                               self.sprite_size, self.sprite_size,
+                               self.pos[0], self.pos[1])
+        if self.update_frame % 10 == 0:
+            if self.frame_flag:
+                self.frame += 1
+                if self.frame == 4:
+                    self.frame_flag = False
+            else:
+                self.frame -= 1
+                if self.frame == 0:
+                    self.frame_flag = True
+    def move(self):
+        self.pos[0] += self.dir * 5
+
+    def update(self):
+        self.update_frame += 1
+        self.draw()
+        self.move()
+
+
+
 def handle_events():
-    # fill here
+    global running
+    global P1
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                P1.dir += 1
+            elif event.key == SDLK_LEFT:
+                P1.dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                P1.dir -= 1
+            elif event.key == SDLK_LEFT:
+                P1.dir += 1
+
     pass
 
 open_canvas()
 sprite_sheet = load_image('Resource/Image/sprite_sheet.png')
 
 running = True
-
-class Player:
-    def __init__(self):
-        self.pos = [90, 90]   # x, y 위치
-        self.frame = 0
-        self.frame_flag = True  # True 면 프레임 +, False 면 프레임 -
-        self.sprite_size = 66
-    def draw(self):
-        sprite_sheet.clip_draw(self.frame * self.sprite_size,
-                               885 - (266+self.sprite_size),
-                               self.sprite_size, self.sprite_size,
-                               self.pos[0], self.pos[1])
-    def update(self):
-        self.draw()
-        if self.frame_flag:
-            self.frame += 1
-            if self.frame == 4:
-                self.frame_flag = False
-        else:
-            self.frame -= 1
-            if self.frame == 0:
-                self.frame_flag = True
-
 P1 = Player()
 
 while running:
@@ -41,7 +68,7 @@ while running:
 
     update_canvas()
     handle_events()
-    delay(0.1)
+    delay(0.01)
 
 close_canvas()
 
