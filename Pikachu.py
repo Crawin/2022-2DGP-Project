6379acb60b4f667ebd64c931aadc7a81146d81ea
Lee Seung Hak <99tmdgkr@gmail.com>
2022-10-11 20:12:16 +0900

    점프 구현 (점프속도 20)

--- Pikachu.py
+++ Pikachu.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+Jump_Speed = 20
 class Player:
     def __init__(self):
         self.pos = [90, 90]   # x, y 위치
@@ -7,7 +7,7 @@ class Player:
         self.update_frame = 0   # 입력 딜레이가 0.01이여야 조작감이 좋아서 애니메이션은 딜레이가 0.1이 되도록 하는 변수
         self.frame_flag = True  # True 면 프레임 +, False 면 프레임 -
         self.sprite_size = 66
-        self.dir = 0            # +면 우측이동, -면 좌측이동
+        self.dir = [0, 0]            # [+면 우측이동 -면 좌측이동, 점프 스피드]
     def draw(self):
         sprite_sheet.clip_draw(self.frame * self.sprite_size,
                                885 - (266+self.sprite_size),
@@ -23,7 +23,13 @@ class Player:
                 if self.frame == 0:
                     self.frame_flag = True
     def move(self):
-        self.pos[0] += self.dir * 5
+        self.pos[0] += self.dir[0] * 5      # 좌우이동
+        self.pos[1] += self.dir[1]          # 점프
+        if self.pos[1] > 90:
+            self.dir[1] -= 1
+        else:
+            self.dir[1] = 0
+
 
     def update(self):
         self.update_frame += 1
@@ -41,16 +47,19 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                P1.dir += 1
+                P1.dir[0] += 1
             elif event.key == SDLK_LEFT:
-                P1.dir -= 1
+                P1.dir[0] -= 1
+            elif event.key == SDLK_UP:
+                if P1.pos[1] == 90:
+                    P1.dir[1] = Jump_Speed
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                P1.dir -= 1
+                P1.dir[0] -= 1
             elif event.key == SDLK_LEFT:
-                P1.dir += 1
+                P1.dir[0] += 1
 
     pass
 
