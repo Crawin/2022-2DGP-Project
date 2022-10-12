3ebb8c3e161da73e24be36178254efd235b589bb
Lee Seung Hak <99tmdgkr@gmail.com>
2022-10-13 00:32:58 +0900

    Player.update 안에 Player를 draw 하는 것까지 합침

--- Pikachu.py
+++ Pikachu.py
@@ -1,46 +1,14 @@
 from pico2d import *
 import Player
 import Sprite
-Jump_Speed = 20
-
-def handle_events():
-    global running
-    global P1
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                P1.dir[0] += 1
-            elif event.key == SDLK_LEFT:
-                P1.dir[0] -= 1
-            elif event.key == SDLK_UP:
-                if P1.pos[1] == 90:
-                    P1.dir[1] = Jump_Speed
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                P1.dir[0] -= 1
-            elif event.key == SDLK_LEFT:
-                P1.dir[0] += 1
-    pass
 
 open_canvas()
-
 Sprite.load_sprites()
-
 running = True
-P1 = Player.character()
-
+Player.enter()
 while running:
-    clear_canvas()
-
-    P1.update()
-
-    update_canvas()
-    handle_events()
+    Player.handle_events()
+    Player.update()
     delay(0.01)
 
 close_canvas()
--- Player.py
+++ Player.py
@@ -1,6 +1,9 @@
 from pico2d import *
 import Sprite
-class character:
+
+Jump_Speed = 20
+
+class player:
     def __init__(self):
         self.pos = [90, 90]   # x, y 위치
         self.frame = 0
@@ -88,3 +91,42 @@ class character:
         self.update_frame += 1
         self.draw()
         self.move()
+
+P1 = None
+
+def enter():
+    global P1
+    P1 = player()
+
+def exit():
+    global P1
+    del P1
+
+def handle_events():
+    global running
+    global P1
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                P1.dir[0] += 1
+            elif event.key == SDLK_LEFT:
+                P1.dir[0] -= 1
+            elif event.key == SDLK_UP:
+                if P1.pos[1] == 90:
+                    P1.dir[1] = Jump_Speed
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                P1.dir[0] -= 1
+            elif event.key == SDLK_LEFT:
+                P1.dir[0] += 1
+    pass
+
+def update():
+    clear_canvas()
+    P1.update()
+    update_canvas()
