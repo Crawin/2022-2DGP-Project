82782879f4a8d60dee7a4c9613b3028e63ad76b9
Lee Seung Hak <99tmdgkr@gmail.com>
2022-10-16 22:57:53 +0900

    공 추가

--- Pikachu.py
+++ Pikachu.py
@@ -1,17 +1,21 @@
 from pico2d import *
 import Player
 import Sprite
-
-
+import Ball
+import Map
 # -----------------------------------
-open_canvas()
+open_canvas(448, 448)
 Sprite.load_sprites()
 running = True
 Player.enter()
+Ball.enter()
 while running:
+    Map.draw()
     Player.handle_events()
     Player.update()
+    Ball.update()
     delay(0.01)
+Ball.exit()
 Player.exit()
 close_canvas()
 # -----------------------------------
