da0f694aa733e877d956ae7f7d942feb9ef5ae7f
Lee Seung Hak <99tmdgkr@gmail.com>
2022-10-16 22:57:24 +0900

    공 크기 선언

--- Sprite.py
+++ Sprite.py
@@ -2,6 +2,7 @@ from pico2d import *
 
 sprite_sheets = [None, None]
 sprite_size = 66
+ball_size = 42
 def load_sprites():
     global sprite_sheets
     sprite_sheets = [load_image('Resource/Image/sprite_sheet.png'), load_image('Resource/Image/sprite_sheet_reverse.png')]
