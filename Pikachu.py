fc29c67608f030e84935580c331d75e91e262ee7
Crawin <99tmdgkr@gmail.com>
2022-10-17 15:34:55 +0900

    Scene.py를 만들어 Scene에 따라 오브젝트를 관리하도록 만듬

--- Pikachu.py
+++ Pikachu.py
@@ -3,22 +3,28 @@ import Player
 import Sprite
 import Ball
 import Map
+import Scene
+
 # -----------------------------------
 open_canvas(448, 448)
 Sprite.load_sprites()
 running = True
-Map.enter()
-Player.enter()
-Ball.enter()
+Scene.enter('play')
+# Map.enter()
+# Player.enter()
+# Ball.enter()
 while running:
+    Scene.handle_events('play')
+    Scene.draw('play')
     Map.draw()
-    Player.handle_events()
+    # Player.handle_events()
     Player.update()
     Ball.update()
     delay(0.01)
 
-Ball.exit()
-Player.exit()
-Map.exit()
+Scene.exit()
+# Ball.exit()
+# Player.exit()
+# Map.exit()
 close_canvas()
 # -----------------------------------
---
+++ Scene.py
@@ -0,0 +1,65 @@
+from pico2d import *
+import Player
+import Map
+import Player
+import Ball
+
+
+def handle_events(type):
+    match type:
+        case 'play':
+            events = get_events()
+            for event in events:
+                if event.type == SDL_KEYDOWN:
+                    match event.key:
+                        case pico2d.SDLK_g:
+                            Player.P1.dir[0] += 1
+                        case pico2d.SDLK_d:
+                            Player.P1.dir[0] -= 1
+                        case pico2d.SDLK_r:
+                            if Player.P1.pos[1] == Player.floor:
+                                if Player.P1.motion != 'dive':
+                                    Player.P1.motion = 'jump'
+                            Player.P1.dir[2] = True
+                        case pico2d.SDLK_z:
+                            if Player.P1.motion == 'idle':
+                                Player.P1.motion = 'dive'
+                            elif Player.P1.motion == 'jump':
+                                Player.P1.motion = 'spike'
+                elif event.type == SDL_KEYUP:
+                    match event.key:
+                        case pico2d.SDLK_g:
+                            Player.P1.dir[0] -= 1
+                        case pico2d.SDLK_d:
+                            Player.P1.dir[0] += 1
+                        case pico2d.SDLK_r:
+                            Player.P1.dir[2] = False
+            pass
+        case 'main':
+            pass
+
+def draw(type):
+    match type:
+        case 'play':
+            pass
+        case 'main':
+            pass
+
+def enter(type):
+    match type:
+        case 'play':
+            Map.enter()
+            Player.enter()
+            Ball.enter()
+            pass
+        case 'main':
+            pass
+
+def exit(type):
+    match type:
+        case 'play':
+            Ball.exit()
+            Player.exit()
+            Map.exit()
+        case 'main':
+            pass
